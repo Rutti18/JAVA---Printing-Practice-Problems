dee119b71565bbccf9ed34b9e794af4d5de07326
Rutti18 <93547129+Rutti18@users.noreply.github.com>
2022-05-02 16:13:51 -0400

    Delete HiFriend.java

--- HiFriend/HiFriend/src/HiFriend.java
+++
@@ -1,7 +0,0 @@
-public class HiFriend {
-    public static void main(String[] args) throws Exception {
-        String strName = "Hi"+" Larry"+" Sprinkle";
-        System.out.println(strName);
-
-    }
-}
