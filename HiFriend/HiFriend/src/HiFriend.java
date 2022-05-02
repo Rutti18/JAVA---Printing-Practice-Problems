26e471495101219e355008f9e22c610c7215a3f3
Rutti18 <93547129+Rutti18@users.noreply.github.com>
2022-05-02 13:09:25 -0400

    Add files via upload
    Added HiFriend project

---
+++ HiFriend/HiFriend/src/HiFriend.java
@@ -0,0 +1,7 @@
+public class HiFriend {
+    public static void main(String[] args) throws Exception {
+        String strName = "Hi"+" Larry"+" Sprinkle";
+        System.out.println(strName);
+
+    }
+}
